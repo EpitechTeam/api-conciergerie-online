beb47f8af95538c0540fdd389941e16906cc66ab
dylan <dylan@libertymedical.fr>
2020-01-20 19:01:15 +0100

    test front

--- src/services/user/user.js
+++ src/services/user/user.js
@@ -137,7 +137,7 @@ let forgot = async (req, res) => {
                         "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
                         "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
                         "http://" +
-                        req.headers.host +
+                        process.env.CONCIERGERIE +
                         "/reset/" +
                         passwordResetToken +
                         "\n\n" +
