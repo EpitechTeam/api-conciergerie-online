972a36dd7fb0359f9f982d5c171b1a5a1866d8bf
acherkaoui <abderrahim.cherkaoui@epitech.eu>
2020-01-21 12:30:35 +0100

    getmissionbycity

--- src/routers/mission.js
+++ src/routers/mission.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const missionService = require('../services/algolia/mission')
 
 router.put('/mission', missionService.insertMission)
-router.post('/getmissionbyname', missionService.findMissionByCity)
+router.post('/getmissionbycity', missionService.findMissionByCity)
 router.delete('/mission', missionService.deleteMissionById)
 
 module.exports = router
