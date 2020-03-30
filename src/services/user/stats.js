171de73a366529048ba264dd6879f7e71fdaa72f
dylan <dylan@libertymedical.fr>
2020-03-30 10:52:54 +0200

    stats

--- src/services/user/stats.js
+++ src/services/user/stats.js
@@ -28,17 +28,112 @@ let getStats = async (req, res) => {
     const token = req.header('Authorization').replace('Bearer ', '');
     const data = jwt.verify(token, process.env.JWT_KEY);
     try {
-        const mission = await Mission.find({user_id: data._id});
+        const mission = await Mission.find({ user_id: data._id });
         if (!mission) {
             throw new Error()
         }
         console.log("data=>", getStatsModel(mission));
-        res.status(200).send(getStatsModel(mission))
+        res.status(200).send(mission)//getStatsModel(mission))
     } catch (error) {
-        res.status(401).send({error: error})
+        res.status(401).send({ error: error })
+    }
+}
+
+let getMonthlyDeals = async (req, res) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const data = jwt.verify(token, process.env.JWT_KEY)
+        const month = req.body.month
+        const mission = await Mission.aggregate(
+            [
+                {
+                    "$match": {
+                        "user_id": { "$regex": data._id, "$options": "i" },
+                    }
+                },
+                {
+                    "$unwind": "$user_id"
+                },
+                {
+                    "$match": {
+                        "user_id": { "$regex": data._id, "$options": "i" },
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": "$_id",
+                        "date": { "$first": "$date" },
+                        "deal": { "$first": "$deal" }
+                    }
+                }
+            ]
+        )
+        const add = (a, b) => a + b
+        if (mission) {
+            let deals = []
+            for (let i = 0; i < mission.length; i++) {
+                const splitedDate = mission[i].date.split("/")
+                if (month.toString() === splitedDate[1]) {
+                    deals.push(parseInt(mission[i].deal))
+                }
+            }
+            const result = deals.reduce(add)
+            res.status(200).send({ nbMissionPerMonth: deals.length, priceEachMission: deals, totalDeals: result })
+        }
+        else {
+            res.status(401).send({ error: "no mission found for this user" })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(401).send({ error: err })
+    }
+}
+
+let getMonthlyMission = async (req, res) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const data = jwt.verify(token, process.env.JWT_KEY)
+        const month = req.body.month
+        const mission = await Mission.aggregate(
+            [
+                {
+                    "$match": {
+                        "user_id": { "$regex": data._id, "$options": "i" },
+                    }
+                },
+                {
+                    "$unwind": "$user_id"
+                },
+                {
+                    "$match": {
+                        "user_id": { "$regex": data._id, "$options": "i" },
+                    }
+                },
+
+            ]
+        )
+        const add = (a, b) => a + b
+        if (mission) {
+            let deals = []
+            for (let i = 0; i < mission.length; i++) {
+                const splitedDate = mission[i].date.split("/")
+                if (month.toString() === splitedDate[1]) {
+                    deals.push(mission[i])
+                }
+            }
+            res.status(200).send({ nbMissionPerMonth: deals.length, totalMission: deals })
+        }
+        else {
+            res.status(401).send({ error: "no mission found for this user" })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(401).send({ error: err })
     }
 }
 
 module.exports = {
-    getStats
+    getStats,
+    getMonthlyDeals,
+    getMonthlyMission
 };
